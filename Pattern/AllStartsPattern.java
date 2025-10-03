f7c949f164692cb3b7a3ee7436ab343505c54189
Saurabh Kumar <kumarsaurabh8286@gmail.com>
2025-10-03 18:51:15 +0530

    hollow pyramid

--- Pattern/AllStartsPattern.java
+++ Pattern/AllStartsPattern.java
@@ -119,6 +119,31 @@ public class AllStartsPattern {
 
         System.out.println();
     }
+    
+    static void HollowPyramidStar(int n) {
+
+        System.out.println("Hollow Pyramid Star Pattern ");
+
+        int spaces = n-1;
+        int stars = 1;
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=spaces; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=stars; j++){
+                if(j==1 || j==stars || i==n)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+            spaces--;
+            stars += 2;
+        }
+
+        System.out.println();
+    }
 
     public static void main(String[] args) {
         
@@ -133,7 +158,8 @@ public class AllStartsPattern {
         // MirroredRightTriangleStar(n);
         // InvertedRightTriangleStar(n);
         // InvertedMirroredRightTriangleStar(n);
-        PyramidStar(n);
+        // PyramidStar(n);
+        HollowPyramidStar(n);
 
     }
 }
