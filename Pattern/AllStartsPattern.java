8948bafb94289c11c1577f6958fce8b37f738394
Saurabh Kumar <kumarsaurabh8286@gmail.com>
2025-10-04 21:10:27 +0530

    hollow

--- Pattern/AllStartsPattern.java
+++ Pattern/AllStartsPattern.java
@@ -32,7 +32,6 @@ public class AllStartsPattern {
         System.out.println();
     }
 
-
     static void RightTriangleStar(int n) {
 
         System.out.println("Right Triangle Star Pattern ");
@@ -144,6 +143,67 @@ public class AllStartsPattern {
 
         System.out.println();
     }
+    
+    static void DiamondStar(int n) {
+
+        System.out.println("Diamond Star Pattern ");
+
+        int spaces = n/2;
+        int stars = 1;
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=spaces; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=stars; j++){
+               System.out.print("*");
+            }
+            System.out.println();
+
+            if (i <= n/2) {
+                spaces--;
+                stars += 2;
+            }
+            else {
+                spaces++;
+                stars -= 2;
+            }
+        }
+
+        System.out.println();
+    }
+    
+    static void HollowDiamondStar(int n) {
+
+        System.out.println("Hollow Diamond Star Pattern ");
+
+        int spaces = n/2;
+        int stars = 1;
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=spaces; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=stars; j++){
+                if(j==1 || j==stars)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+
+            if (i <= n/2) {
+                spaces--;
+                stars += 2;
+            }
+            else {
+                spaces++;
+                stars -= 2;
+            }
+        }
+
+        System.out.println();
+    }
 
     public static void main(String[] args) {
         
@@ -159,7 +219,9 @@ public class AllStartsPattern {
         // InvertedRightTriangleStar(n);
         // InvertedMirroredRightTriangleStar(n);
         // PyramidStar(n);
-        HollowPyramidStar(n);
+        // HollowPyramidStar(n);
+        // DiamondStar(n);
+        HollowDiamondStar(n);
 
     }
 }
