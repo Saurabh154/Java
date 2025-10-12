63a1fa1c476b5245a8d221f8957c40bcb02963fc
Saurabh Vishwakarma <kumarsaurabh8286@gmail.com>
2025-10-12 20:57:10 +0530

    constructor

---
+++ Oops/Constructor/A.java
@@ -0,0 +1,23 @@
+package Oops.Constructor;
+
+class InnerA {
+    int i,j;
+    InnerA(){
+        System.out.println("Constructor");
+    }
+
+    InnerA(int a, int b){
+        this();
+        i=a;
+        j=b;
+
+    }
+    
+}
+public class A {
+    public static void main(String[] args) {
+        InnerA a1 = new InnerA(10,20);
+        System.out.println(a1.i);
+        System.out.println(a1.j);
+    }
+}
